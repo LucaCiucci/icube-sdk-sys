4f93e8865ecce168e0f28084dc874a33780cab04
LucaCiucci <luca.ciucci99@gmail.com>
2024-08-21 13:31:25 +0200

    chore: update example

--- examples/a.rs
+++ examples/a.rs
@@ -8,4 +8,14 @@ fn main() {
         SDK::V1(_) => println!("Using the iCube SDK v1 (NETUSBCAM)"),
         SDK::V2(_) => println!("Using the iCube SDK v2"),
     }
+
+    let SDK::V1(api) = api else {
+        panic!()
+    };
+
+    let num_devices = unsafe {
+        (api.Init)()
+    };
+
+    println!("Found {} cameras", num_devices);
 }
